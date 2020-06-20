577fe87480f687cc924dc36569251fdea68c7f0b
MestreLion <linux@rodrigosilva.com>
2020-06-20 13:04:15 -0300

    add model, DAO and persistence with H2 memory database

--- src/main/java/com/rodrigosilva/ap3/view/Bean.java
+++ src/main/java/com/rodrigosilva/ap3/view/Bean.java
@@ -1,33 +1,41 @@
 package com.rodrigosilva.ap3.view;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import com.rodrigosilva.ap3.model.Message;
+import com.rodrigosilva.ap3.service.MessageService;
 
 @Named
 @RequestScoped
 public class Bean {
-	private String input;
-	private String output;
+	private Message message = new Message();
+	private List<Message> messages;
+
+	@Inject
+	private MessageService messageService;
+
+	@PostConstruct
+	public void init() {
+		messages = messageService.list();
+	}
 
 	public void submit() {
-		output = "Hello World! You have typed: " + input;
+		messageService.create(message);
+		messages.add(message);
+		message = new Message();
 	}
 
-	////////////////////////////////////////////
+	//////////////////////////////////////////////
 
-	public String getInput() {
-		return input;
+	public Message getMessage() {
+		return message;
 	}
-
-	public void setInput(String input) {
-		this.input = input;
-	}
-
-	public String getOutput() {
-		return output;
-	}
-
-	public void setOutput(String output) {
-		this.output = output;
+	public List<Message> getMessages() {
+		return messages;
 	}
 }
